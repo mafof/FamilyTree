ca86ad8f276597516c48806c885436d37cde1ebd
mafof <pavel.bykov@mafof.ru>
2023-02-24 03:47:40 +0300

    Адаптировал валидацию форм под ASP.NET

--- FamilyTree/wwwroot/js/site.js
+++ FamilyTree/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿
+﻿// JS валидация форм =>
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.custom-form')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
