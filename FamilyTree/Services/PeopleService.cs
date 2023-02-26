8e483c58f0f865addc68151ecbb842ebfa280e1a
mafof <pavel.bykov@mafof.ru>
2023-02-26 14:29:19 +0300

    Добавил сервис для моделей

---
+++ FamilyTree/Services/PeopleService.cs
@@ -0,0 +1,23 @@
+﻿using FamilyTree.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyTree.Services
+{
+    public class PeopleService : DbContext
+    {
+        public PeopleService()
+        { }
+
+        public PeopleService(DbContextOptions<PeopleService> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost\\MSSQLSERVER01;Database=test;User Id=mafof;password=mafof;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=False;TrustServerCertificate=False;");
+        }
+
+        public DbSet<PeopleModel> People { get; set; } = null!;
+        public DbSet<LinkModel> Link { get; set; } = null!;
+    }
+}
