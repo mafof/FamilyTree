a4d3900e4bf745d454ad0628a2e42435f565a020
mafof <pavel.bykov@mafof.ru>
2023-02-26 14:29:10 +0300

    Добавил модели

---
+++ FamilyTree/Models/LinkModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FamilyTree.Models
+{
+    public class LinkModel
+    {
+        [Required] 
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("PeopleMain")]
+        public int PeopleId { get; set; }
+
+        [Required]
+        public int PeopleChildID { get; set; }
+
+        [Required]
+        public int Level { get; set; }
+
+        public PeopleModel PeopleMain { get; set; } = null!;
+    }
+}
---
+++ FamilyTree/Models/PeopleModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FamilyTree.Models
+{
+    public class PeopleModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Surname { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public ICollection<LinkModel> Links { get; set; } = null!;
+    }
+}
