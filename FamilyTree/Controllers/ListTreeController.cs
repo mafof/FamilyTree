b96de79e4afc364676798cf4a51a77da01547f1a
mafof <pavel.bykov@mafof.ru>
2023-02-27 21:16:12 +0300

    Добавил обработку у контроллера отчетов

--- FamilyTree/Controllers/ListTreeController.cs
+++ FamilyTree/Controllers/ListTreeController.cs
@@ -2,24 +2,39 @@
 using FamilyTree.Models;
 using FamilyTree.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Data.Entity.Infrastructure;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using System.Diagnostics;
 
 namespace FamilyTree.Controllers
 {
     public class ListTreeController : Controller
-    { 
-
-        public IActionResult Index(int? id)
+    {
+        [HttpGet("[controller]/[action]/{id}")]
+        public IActionResult Index(int id, string? surname, string? name, string? patronymic)
         {
             using(PeopleService db = new PeopleService())
             {
-                var s = db.GrandMotherGreatGrandson(14);
-                Debug.WriteLine(s);
 
+                List<GrandMotherGreatGrandsonModel> resultDB = db.GrandMotherGreatGrandson(id).ToList();
+
+                List<List<GrandMotherGreatGrandsonModel>> result = new List<List<GrandMotherGreatGrandsonModel>>();
+
+                int? idGrandmother = null;
+                resultDB.ForEach(el =>
+                {
+                    if (idGrandmother == null || idGrandmother != el.IdGrandmother) 
+                    {
+                        idGrandmother = el.IdGrandmother;
+                        result.Add(new List<GrandMotherGreatGrandsonModel>());
+
+                    }
+                    
+                    result[result.Count - 1].Add(el);
+                });
+
+                ViewBag.FullName = $"{surname} {name} {patronymic}";
+                ViewBag.Result = result;
             }
-
             return View();
         }
     }
